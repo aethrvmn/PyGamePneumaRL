83ab08326eba94b2120724e39b56f589a5d4596e
Vasilis Valatsos <vasilvalat@gmail.com>
2023-09-03 15:58:28 +0300

    Major restructuring ongoing

---
+++ game/configs/game/spell_config.py
@@ -0,0 +1,3 @@
+magic_data = {
+	'flame': {'strength': 5,'cost': 20,'graphic':'../Graphics/particles/flame/fire.png'},
+	'heal' : {'strength': 20,'cost': 10,'graphic':'../Graphics/particles/heal/heal.png'}}
---
+++ game/configs/game/weapon_config.py
@@ -0,0 +1,6 @@
+weapon_data = {
+	'sword': {'cooldown': 100, 'damage': 15,'graphic':'../Graphics/weapons/sword/full.png'},
+	'lance': {'cooldown': 400, 'damage': 30,'graphic':'../Graphics/weapons/lance/full.png'},
+	'axe': {'cooldown': 300, 'damage': 20, 'graphic':'../Graphics/weapons/axe/full.png'},
+	'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'../Graphics/weapons/rapier/full.png'},
+	'sai':{'cooldown': 80, 'damage': 10, 'graphic':'../Graphics/weapons/sai/full.png'}}
