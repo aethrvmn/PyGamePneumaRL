a730ca7a8440bc6d63796eb49ada9522a02fc280
Vasilis Valatsos <vasilvalat@gmail.com>
2024-05-22 08:44:04 +0200

    Moved Godot version to own repo for clarity

--- Godot/main.py
+++
@@ -1,6 +0,0 @@
-from policy import policies
-from agent import main
-import args
-
-for policy_name, policy in policies.items():
-    main(policy_name=policy_name, policy=policy, parseargs=args.parse_args())
--- Godot/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python main.py --env_path="/home/valapeos/Projects/thesis/Godot/pneuma.x86_64" --speedup=200 --n_parallel=4 --exper_dir="logs/sb3_full_1" &&
-
-python main.py --env_path="/home/valapeos/Projects/thesis/Godot/pneuma.x86_64" --speedup=200 --n_parallel=4 --exper_dir="logs/sb3_full_2" &&
-
-python main.py --env_path="/home/valapeos/Projects/thesis/Godot/pneuma.x86_64" --speedup=200 --n_parallel=4 --exper_dir="logs/sb3_full_3" &&
-
-python main.py --env_path="/home/valapeos/Projects/thesis/Godot/pneuma.x86_64" --speedup=200 --n_parallel=4 --exper_dir="logs/sb3_full_4"
